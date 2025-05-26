eef9def39627b5b5d999181b67bbb35d6b86a0de
MGSimard <mgsimard.dev@gmail.com>
2025-05-26 00:06:51 -0400

    Collapse stuff

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 import { Draggable } from "gsap/Draggable";
@@ -9,10 +9,12 @@ import { ToggleTheme } from "./ToggleTheme";
 import { ToggleBackground } from "./ToggleBackground";
 import { ToggleLineNumbers } from "./ToggleLineNumbers";
 import { TogglePreview } from "./TogglePreview";
+import { IconEllipsis, IconMinus } from "@/components/Icons";
 
 gsap.registerPlugin(useGSAP, Draggable, Flip, InertiaPlugin);
 
 export function Toolbar() {
+  const [isOpen, setIsOpen] = useState(true);
   const toolbarRef = useRef<HTMLDivElement>(null);
 
   useGSAP(() => {
@@ -27,17 +29,28 @@ export function Toolbar() {
   });
 
   return (
-    <div id="toolbar" ref={toolbarRef} role="toolbar">
+    <div id="toolbar" ref={toolbarRef} role="toolbar" data-open={isOpen}>
       <div id="toolbar-inner">
-        <div id="toolbar-left">
-          <div id="toolbar-left-inner"></div>
+        <div id="toolbar-trim">
+          <div id="toolbar-trim-inner">
+            <button
+              id="toolbar-control"
+              type="button"
+              aria-label="Close toolbar"
+              className="icon-button"
+              onClick={() => setIsOpen(!isOpen)}>
+              {isOpen ? <IconMinus aria-hidden="true" /> : <IconEllipsis aria-hidden="true" />}
+            </button>
+          </div>
         </div>
-        <div id="toolbar-right">
-          <ToggleTheme />
-          <ToggleBackground />
-          <ToggleLineNumbers />
-          <CyclePadding />
-          <TogglePreview />
+        <div id="toolbar-content" inert={isOpen ? false : true}>
+          <div>
+            <ToggleTheme />
+            <ToggleBackground />
+            <ToggleLineNumbers />
+            <CyclePadding />
+            <TogglePreview />
+          </div>
         </div>
       </div>
     </div>
